95048906f1ad5043144f1bd9acb1487b474b9847
David Caro <david.caro@cern.ch>
2016-07-08 21:27:15 +0200

    deploy:prepare travis and deploy script autodeploy
    * Adapt the script to handle secret vars
    * Allow branches with dev.{branch}* tags for them
    
    Signed-off-by: David Caro <david.caro@cern.ch>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,33 @@
-#!/bin/bash
+#!/bin/bash -e
 
-echo "Pushing ${TAG}"
-# docker login --password=$DOCKER_PASSWORD --email=$DOCKER_EMAIL --username=$DOCKER_USERNAME
-# docker push ${TAG}
+
+if [[ "$DOCKER_USERNAME" == "" ]] || [[ "$TRAVIS_PULL_REQUEST" == "true" ]]; then
+    echo "Not in an official branch, skipping deploy"
+    echo "TRAVIS_SECURE_ENV_VARS=$TRAVIS_SECURE_ENV_VARS"
+    echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+    exit 0
+fi
+
+# fail on unset variables expansion
+set -o nounset
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    REMOTE_TAG="$DOCKER_PROJECT:$DOCKER_IMAGE_TAG"
+else
+    REMOTE_TAG="$DOCKER_PROJECT:dev.$TRAVIS_BRANCH-$DOCKER_IMAGE_TAG"
+fi
+
+echo "Logging into Docker Hub with user $DOCKER_USERNAME"
+docker login \
+    "--password=$DOCKER_PASSWORD" \
+    "--email=$DOCKER_EMAIL" \
+    "--username=$DOCKER_USERNAME"
+
+echo "Tagging already build docker image $TAG with $REMOTE_TAG"
+docker tag "$TAG" "$REMOTE_TAG"
+
+echo "Pushing image to ${REMOTE_TAG}"
+docker push ${REMOTE_TAG}
+
+echo "Logging out"
+docker logout
