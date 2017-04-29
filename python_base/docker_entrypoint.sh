6b337423211608f96bda3501256b0e39bc595a43
David Caro <david@dcaro.es>
2017-04-29 19:28:14 +0200

    python_base: Fix the venv permisions on entry/exit
    * Now it will run fix_venv_rights both on entry and exit, the fact
      that we have to run it on exit required not using `exec`, and
      rewriting part of the code to be able to forward signals to the
      child proces, that ended up in a bit of refactoring to group all the
      code in functions too.
    
    Signed-off-by: David Caro <david@dcaro.es>

--- python_base/docker_entrypoint.sh
+++ python_base/docker_entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-
+#!/bin/bash -ei
+#
 # -*- coding: utf-8 -*-
 #
 # This file is part of INSPIRE.
-# Copyright (C) 2016 CERN.
+# Copyright (C) 2017 CERN.
 #
 # INSPIRE is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,21 +21,73 @@
 # In applying this licence, CERN does not waive the privileges and immunities
 # granted to it by virtue of its status as an Intergovernmental Organization
 # or submit itself to any jurisdiction.
-
-set -e
+#
+# Note on the usage of signal handlers.
+#
+# In order to be able to restore the ownership of the venv at the end, we have
+# to declare it as a trap for the EXIT signal, that forces us to not use
+# 'exec' for the wrapped command, and that leads us to manually having to
+# forward also the SIGTERM and SIGINT signals.
+set -me
 
 VENV_PATH=/virtualenv
 
-if ! [[ -f "$VENV_PATH/bin/activate" ]]; then
+
+restore_venv_rights() {
+    if [[ "$BASE_USER_UID" != "" ]]; then
+        BASE_USER_GID="${BASE_USER_GID:-$BASE_USER_UID}"
+        echo "Restoring permissions of venv to $BASE_USER_UID:$BASE_USER_GID"
+        /fix_venv_rights "$BASE_USER_UID:$BASE_USER_GID"
+    else
+        echo "No BASE_USER_UID env var defined, skipping venv permission" \
+            "restore."
+    fi
+}
+
+forward_sigterm() {
+    echo "Forwarding SIGTERM to $child"
+    kill -SIGTERM "$child" &>/dev/null
+    trap forward_sigterm SIGTERM
+    wait "$child"
+}
+
+
+forward_sigint() {
+    echo "Forwarding SIGINT to $child"
+    kill -SIGINT "$child" &>/dev/null
+    trap forward_sigint SIGINT
+    wait "$child"
+}
+
+
+prepare_venv() {
     virtualenv "$VENV_PATH" -p "python${INSPIRE_PYTHON_VERSION}"
     source "$VENV_PATH"/bin/activate
     pip install --upgrade pip
     pip install --upgrade setuptools wheel
     cp -r /src-cache "$VENV_PATH"/src
-else
-    source "$VENV_PATH"/bin/activate
-fi
+}
 
-find \( -name __pycache__ -o -name '*.pyc' \) -delete
 
-exec "$@"
+main() {
+    /fix_venv_rights 'test:test'
+    trap restore_venv_rights EXIT
+
+    if ! [[ -f "$VENV_PATH/bin/activate" ]]; then
+        prepare_venv
+    else
+        source "$VENV_PATH"/bin/activate
+    fi
+
+    find \( -name __pycache__ -o -name '*.pyc' \) -delete
+
+    trap forward_sigterm SIGTERM
+    trap forward_sigint SIGINT
+
+    "$@" &
+    child="$!"
+    fg >/dev/null
+}
+
+
+main "$@"
