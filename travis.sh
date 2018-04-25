878669b2e3fd228e291cdfbb12504711facf3940
Szymon Łopaciuk <szymon.lopaciuk@cern.ch>
2018-04-25 11:39:27 +0200

    create python-base-test image with mitmproxy CA
    Signed-off-by: Szymon Łopaciuk <szymon.lopaciuk@cern.ch>

---
+++ travis.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -ev
+
+conf_python_base_versioned() {
+    export DOCKER_PROJECT=inspirehep/python-base
+    export DOCKER_IMAGE_TAG=2.7
+    export DOCKERFILE=python_base/Dockerfile
+    export ARGS='--build-arg=INSPIRE_PYTHON_VERSION=2.7'
+}
+
+conf_python_base_latest() {
+    export DOCKER_PROJECT=inspirehep/python-base
+    export DOCKER_IMAGE_TAG=latest
+    export DOCKERFILE=python_base/Dockerfile
+    export ARGS='--build-arg=INSPIRE_PYTHON_VERSION=2.7'
+}
+
+
+conf_python_base_test_versioned() {
+    export DOCKER_PROJECT=inspirehep/python-base-test
+    export DOCKER_IMAGE_TAG=2.7
+    export DOCKERFILE=python_base_test/Dockerfile
+    export ARGS='--build-arg=INSPIRE_PYTHON_VERSION=2.7'
+}
+
+conf_python_base_test_latest() {
+    export DOCKER_PROJECT=inspirehep/python-base-test
+    export DOCKER_IMAGE_TAG=latest
+    export DOCKERFILE=python_base_test/Dockerfile
+    export ARGS='--build-arg=INSPIRE_PYTHON_VERSION=2.7'
+}
+
+conf_elasticsearch() {
+    export DOCKER_PROJECT=inspirehep/elasticsearch
+    export DOCKER_IMAGE_TAG=latest
+    export DOCKERFILE=elasticsearch/Dockerfile
+    export ARGS=''
+}
+
+conf_python_base_versioned; ./build.sh
+conf_python_base_latest; ./build.sh
+conf_python_base_test_versioned; ./build.sh
+conf_python_base_test_latest; ./build.sh
+conf_elasticsearch; ./build.sh
+
+./test.sh
+
+conf_python_base_versioned; ./deploy.sh
+conf_python_base_latest; ./deploy.sh
+conf_python_base_test_versioned; ./deploy.sh
+conf_python_base_test_latest; ./deploy.sh
+conf_elasticsearch; ./deploy.sh
