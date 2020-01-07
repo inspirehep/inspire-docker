cfc65620b18bd7b59755f8b6feebc2ae1f18330a
Harris Tzovanakis <me@drjova.com>
2020-01-07 10:56:14 +0100

    global: addition of elasticsearch 7
    Signed-off-by: Harris Tzovanakis <me@drjova.com>

--- travis.sh
+++ travis.sh
@@ -43,12 +43,20 @@ conf_elasticsearch5() {
     export ARGS=''
 }
 
+conf_elasticsearch7() {
+    export DOCKER_PROJECT=inspirehep/elasticsearch7
+    export DOCKER_IMAGE_TAG=latest
+    export DOCKERFILE=elasticsearch7/Dockerfile
+    export ARGS=''
+}
+
 conf_python_base_versioned; ./build.sh
 conf_python_base_latest; ./build.sh
 conf_python_base_test_versioned; ./build.sh
 conf_python_base_test_latest; ./build.sh
 conf_elasticsearch; ./build.sh
 conf_elasticsearch5; ./build.sh
+conf_elasticsearch7; ./build.sh
 
 ./test.sh
 
@@ -58,3 +66,4 @@ conf_python_base_test_versioned; ./deploy.sh
 conf_python_base_test_latest; ./deploy.sh
 conf_elasticsearch; ./deploy.sh
 conf_elasticsearch5; ./deploy.sh
+conf_elasticsearch7; ./deploy.sh
